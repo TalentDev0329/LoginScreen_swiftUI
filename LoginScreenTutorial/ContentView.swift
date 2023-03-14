0629c36ed3c8aa34be8ef94fc0dc56b09e428efc
TalentDev0329 <72875041+TalentDev0329@users.noreply.github.com>
2023-03-14 16:38:49 +0800

    Initial commit

---
+++ LoginScreenTutorial/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  LoginScreenTutorial
+//
+//  Created by Duy Bui on 11/9/19.
+//  Copyright © 2019 iosAppTemplates. All rights reserved.
+//
+import SwiftUI
+
+struct LoginView: View {
+    // MARK: - Propertiers
+    @State private var email = ""
+    @State private var password = ""
+    
+    // MARK: - View
+    var body: some View {
+        VStack() {
+            Text("iOS App Templates")
+                .font(.largeTitle).foregroundColor(Color.white)
+                .padding([.top, .bottom], 40)
+                .shadow(radius: 10.0, x: 20, y: 10)
+            
+            Image("iosapptemplate")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10.0, x: 20, y: 10)
+                .padding(.bottom, 50)
+            
+            VStack(alignment: .leading, spacing: 15) {
+                TextField("Email", text: self.$email)
+//                    .padding()
+                    .padding(.all, 10)
+                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+                
+                SecureField("Password", text: self.$password)
+//                    .padding()
+                    .padding(.all, 10)
+                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding([.leading, .trailing], 27.5)
+            
+            Button(action: {}) {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 45)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding(.top, 50)
+            
+            Spacer()
+            HStack(spacing: 0) {
+                Text("Don't have an account?").padding(.trailing, 10)
+                Button(action: {}) {
+                    Text("Sign Up").foregroundColor(.white)
+                }
+            }
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all))
+        
+    }
+}
+
+extension Color {
+    static var themeTextField: Color {
+        return Color(red: 220.0/255.0, green: 230.0/255.0, blue: 230.0/255.0, opacity: 1.0)
+    }
+}
